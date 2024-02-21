9e3c5e658d1697e25a76bd36388e6683eeeaf2e2
Melki <mortizm28@miumg.edu.gt>
2024-02-21 16:23:25 -0600

    Actualizacion Dia a partir de numero

--- EstructurasCondicionales/Program.cs
+++ EstructurasCondicionales/Program.cs
@@ -25,4 +25,44 @@ switch (numeroEnLetra)
         return;
 }
 Console.WriteLine($"El número en dígito es: {numeroEnNumero}");
+
+//--------------------------------------------------------------------------------------------------------------------
+
+// 2. Mostrar dia de la semana a partir de numero
+Console.WriteLine("*Dia de la semana a partir de un numero*");
+Console.WriteLine("Ingrese un número del 1 al 7 para mostrar el día de la semana:");
+int numeroDia = int.Parse(Console.ReadLine());
+
+string diaSemana = "";
+
+switch (numeroDia)
+{
+    case 1:
+        diaSemana = "Lunes";
+        break;
+    case 2:
+        diaSemana = "Martes";
+        break;
+    case 3:
+        diaSemana = "Miércoles";
+        break;
+    case 4:
+        diaSemana = "Jueves";
+        break;
+    case 5:
+        diaSemana = "Viernes";
+        break;
+    case 6:
+        diaSemana = "Sábado";
+        break;
+    case 7:
+        diaSemana = "Domingo";
+        break;
+    default:
+        Console.WriteLine("Número de día no válido.");
+        return;
+}
+
+Console.WriteLine($"El día de la semana es: {diaSemana}");
+    
     
